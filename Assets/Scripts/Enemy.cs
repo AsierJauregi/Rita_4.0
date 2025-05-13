19f992f4880f2c74f8e9a1924d1b96e04c0f6e33
diegogpage <diegogpage2@gmail.com>
2025-05-13 12:28:54 +0200

    Ataque implementado

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float vida;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitarVida(float danho)
+    {
+        vida -= danho;
+        Debug.Log("Vida restante: " + vida);
+        if (vida <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
